0c68fef91093ce24533ed2c4d4dedd33bde4b8d1
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2022-01-03 18:52:12 -0300

    fix: add title text component instead of body text

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 
 import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 export default function GameOverScreen(props) {
   return (
     <View style={styles.screen}>
-      <BodyText>The Game Is Over!</BodyText>
+      <TitleText>The Game Is Over!</TitleText>
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number to guess: {props.userNumber}</BodyText>
       <Button title="NEW GAME" onPress={props.onRestart} />
